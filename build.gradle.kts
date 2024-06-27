b54b9b370ead2592aeff13702920039182daafe4
megustas2xj <37738743+megustas2xj@users.noreply.github.com>
2024-06-27 21:07:21 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -119,15 +119,3 @@ subprojects {
     }
 }
 
-allprojects {
-    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "17"
-        }
-    }
-    java {
-        toolchain {
-            languageVersion.set(JavaLanguageVersion.of(17))
-        }
-    }
-}
